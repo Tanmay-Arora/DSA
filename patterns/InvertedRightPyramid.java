5db541bccdd4f68785ba1c490c68da40c4536345
@tanmay.arora <tanmay.arora@mavq.com>
2024-01-30 23:36:14 +0530

    InvertedRightPyramid

---
+++ patterns/InvertedRightPyramid.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class InvertedRightPyramid {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            int n = Integer.parseInt(br.readLine());
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<n-i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
